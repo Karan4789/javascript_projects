428c7adf15935fb225f02e614d3e215896846e13
Karan4789 <karan2301.m@gmail.com>
2025-02-10 18:27:31 +0530

    some needs to be done

---
+++ Rock-Paper-Scissor/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+const choices = ["rock","paper","scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerchoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+    
+}
