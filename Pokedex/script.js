5eb97201e8bbccf71ac0215b938c1872691a4682
Karan4789 <karan2301.m@gmail.com>
2025-02-19 21:29:37 +0530

    Styles

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -20,7 +20,11 @@ async function fetchData() {
             const pokename = data.name;
             let displayname = document.getElementById("displayname");
 
+            const elementtype = data.types.map(t => t.type.name).join(",");
+            let type = document.getElementById("type");
+
             displayname.textContent = pokename;
+            type.textContent = "Types: " + elementtype;
 
             imgElement.src = pokeSprites;
             imgElement.style.display = "block"
