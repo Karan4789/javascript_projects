ec8208603e15c935d0aa8f7624aa763949d14197
Karan4789 <karan2301.m@gmail.com>
2025-02-21 22:42:36 +0530

    Weather app

---
+++ Weather App/script.js
@@ -0,0 +1,24 @@
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const cards = document.querySelector(".card");
+const apiKey = "";
+
+weatherForm.addEventListener("submit", event =>{
+
+})
+
+async function getWeatherData(city) {
+    
+}
+
+function displayWeather(data){
+
+}
+
+function getWeatherEmoji(weatherid){
+     
+}
+function displayError(message){
+
+}
